83531754e1002fd9e3d71662b12e8e3c112356f4
Fábio Silva <fabio.fernando.osilva@gmail.com>
2021-01-05 06:52:13 -0300

    Add script to change office apps lang

---
+++ office-mac-ptbr/apply.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+defaults write com.microsoft.Excel AppleLanguages '("pt")'
+defaults write com.microsoft.Word AppleLanguages '("pt")'
+defaults write com.microsoft.Powerpoint AppleLanguages '("pt")'
